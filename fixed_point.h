04816c71722238810d72c0bec141db78ff772ea8
Matthew Wegener <mwegener@protonmail.com>
2019-07-21 17:36:15 -0700

    fixed point macros implemented

---
+++ fixed_point.h
@@ -0,0 +1,39 @@
+#ifndef FIXED_POINT_H
+#define FIXED_POINT_H
+
+/* A Simple C program to illustrate the use of Fixed Point Operations */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* DEFINE THE MACROS */
+/* The basic operations perfomed on two numbers a and b of fixed
+point q format returning the answer in q format */
+#define FADD(a,b) ((a)+(b))
+#define FSUB(a,b) ((a)-(b))
+#define FMUL(a,b,q) (((a)*(b))>>(q))
+#define FDIV(a,b,q) (((a)<<(q))/(b))
+
+/* The basic operations where a is of fixed point q format and b is
+an integer */
+#define FADDI(a,b,q) ((a)+((b)<<(q)))
+#define FSUBI(a,b,q) ((a)-((b)<<(q)))
+#define FMULI(a,b) ((a)*(b))
+#define FDIVI(a,b) ((a)/(b))
+
+/* convert a from q1 format to q2 format */
+#define FCONV(a, q1, q2) (((q2)>(q1)) ? (a)<<((q2)-(q1)) : (a)>>((q1)-(q2)))
+
+/* the general operation between a in q1 format and b in q2 format
+returning the result in q3 format */
+#define FADDG(a,b,q1,q2,q3) (FCONV(a,q1,q3)+FCONV(b,q2,q3))
+#define FSUBG(a,b,q1,q2,q3) (FCONV(a,q1,q3)-FCONV(b,q2,q3))
+#define FMULG(a,b,q1,q2,q3) FCONV((a)*(b), (q1)+(q2), q3)
+#define FDIVG(a,b,q1,q2,q3) (FCONV(a, q1, (q2)+(q3))/(b))
+
+/* convert to and from floating point */
+#define TOFIX(d, q) ((int)( (d)*(double)(1<<(q)) ))
+#define TOFLT(a, q) ( (double)(a) / (double)(1<<(q)) )
+
+
+#endif
