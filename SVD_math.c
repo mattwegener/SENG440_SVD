b1ad0ba0aa155507c20d4842cdac30daba210c22
Noah <ntrondeau@gmail.com>
2019-08-02 08:25:33 -0700

    Multiplication in fixed point now works

--- SVD_math.c
+++ SVD_math.c
@@ -31,7 +31,7 @@
 #define SEG2_SLOPE_C    (-178)
 #define SEG3_SLOPE_C    (-244)
 #define SEG4_SLOPE_C    (-244)
-#define SEG5_SLOPE_C    (-278)
+#define SEG5_SLOPE_C    (-178)
 #define SEG6_SLOPE_C    (-65)
 
 #define SEG1_BIAS_C     (256)
@@ -209,34 +209,48 @@ matrix_elem SVD_abs(matrix_elem x)
 #ifdef TEST
 #include <math.h>
 #include <assert.h>
-matrix_elem x_array[63] = {-12868, -12458, -12049, -11639, -11230, -10820, -10410, -10001, -9591, -9182, -8772, -8362, -7953, -7543, -7134, -6724, -6314, -5905, -5495, -5086, -4676, -4266, -3857, -3447, -3038, -2628, -2218, -1809, -1399, -990, -580, -170, 239, 649, 1058, 1468, 1878, 2287, 2697, 3106, 3516, 3926, 4335, 4745, 5154, 5564, 5974, 6383, 6793, 7202, 7612, 8022, 8431, 8841, 9250, 9660, 10070, 10479, 10889, 11298, 11708, 12118, 12527, };
-matrix_elem atan_arg[81] = {-4096, -3994, -3891, -3789, -3686, -3584, -3482, -3379, -3277, -3174, -3072, -2970, -2867, -2765, -2662, -2560, -2458, -2355, -2253, -2150, -2048, -1946, -1843, -1741, -1638, -1536, -1434, -1331, -1229, -1126, -1024, -922, -819, -717, -614, -512, -410, -307, -205, -102, 0, 102, 205, 307, 410, 512, 614, 717, 819, 922, 1024, 1126, 1229, 1331, 1434, 1536, 1638, 1741, 1843, 1946, 2048, 2150, 2253, 2355, 2458, 2560, 2662, 2765, 2867, 2970, 3072, 3174, 3277, 3379, 3482, 3584, 3686, 3789, 3891, 3994, 4096, };
+matrix_elem x_array[100];
+matrix_elem atan_arg[100];
 
 void TEST_SVD_math()
 {
 
+  float ax = -1.0; // 0.02
+  float sx = -3.141592; // 0.06283185
+
+  for (int i = 0; i < 100; i++)
+  {
+      x_array[i] = TOFIX(sx, Q);
+      sx += 0.06283185;
+
+      atan_arg[i] = TOFIX(ax, Q);
+      ax += 0.02;
+  }
+
     // test the sine
-    for (int i = 0; i < 63; i++)
+    for (int i = 0; i < 100; i++)
     {
       matrix_elem y = SVD_sin(x_array[i]);
       float yf = sinf(TOFLT(x_array[i], Q));
-      printf("x = %f, y = %f, yf = %f\n", TOFLT(x_array[i], Q), TOFLT(y, Q), yf);
-      //assert( fabs(yf - TOFLT(y,Q)) <=  0.05);
-    }
-
-    // test the cosine
-    for (int i = 0; i < 63; i++)
-    {
-      matrix_elem y = SVD_cos(x_array[i]);
-      float yf = cosf(TOFLT(x_array[i], Q));
+      //printf("x = %f, y = %f, yf = %f\n", TOFLT(x_array[i], Q), TOFLT(y, Q), yf);
       assert( fabs(yf - TOFLT(y,Q)) <=  0.05);
     }
 
     // test the cosine
-    for (int i = 0; i < 81; i++)
+    for (int i = 0; i < 100; i++)
+    {
+      matrix_elem y = SVD_cos(x_array[i]);
+      float yf = cosf(TOFLT(x_array[i], Q));
+      //printf("x = %f, y = %f, yf = %f\n", TOFLT(x_array[i], Q), TOFLT(y, Q), yf);
+      assert( fabs(yf - TOFLT(y,Q)) <=  0.05);
+    }
+
+    // test the cosine
+    for (int i = 0; i < 100; i++)
     {
       matrix_elem y = arctan(atan_arg[i]);
       float yf = atanf(TOFLT(atan_arg[i], Q));
+      //printf("x = %f, y = %f, yf = %f\n", TOFLT(atan_arg[i], Q), TOFLT(y, Q), yf);
       assert( fabs(yf - TOFLT(y,Q)) <=  0.05);
     }
 }
