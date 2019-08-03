76f5e113b4f797f907f8ecf4fc7e1e7a2de6313f
Noah <ntrondeau@gmail.com>
2019-08-03 14:58:07 -0700

    Add time profiling

--- profile_main.c
+++ profile_main.c
@@ -1,4 +1,8 @@
 #include "SVD.h"
+#include <time.h>
+#include <stdio.h>
+
+#define ITER       (10000.0f)
 
 matrix Min   = {{  31.0,  77.0,  -11.0,  26.0, },
                 {  -42.0,  14.0, 79.0,  -53.0, },
@@ -20,10 +24,26 @@ matrix Sout  = {{  1.0,  0.0,  0.0,  0.0, },
 
 int main(){
 
+#ifdef TIME_TEST
+    clock_t start, end;
+    double avg_time = 0.0;
+#endif
     // run 1 million times for profiling
-    for (unsigned int i = 0; i < 10000; i++)
+    for (unsigned int i = 0; i < ITER; i++)
     {
+#ifdef TIME_TEST
+        start = clock();
+#endif
         SVD_decompose(Min, Uout, Sout, Vout);
+#ifdef TIME_TEST
+        end = clock();
+        avg_time += (double)(end - start);
+#endif
     }
+
+#ifdef TIME_TEST
+    avg_time = avg_time / ITER;
+    printf("Average time taken (10000 tries): %f\n", avg_time);
+#endif
     return 0;
 }
