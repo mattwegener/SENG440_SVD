9286df8e3be0f6261d2afafa09e00901e9ff5e3d
Noah <ntrondeau@gmail.com>
2019-08-04 15:28:12 -0700

    Eliminate useless function SVD_matrix_dot 2
    Forgot to push some stuff

--- SVD_matrix.h
+++ SVD_matrix.h
@@ -9,7 +9,6 @@
 void SVD_matrix_mul(matrix matrix1, matrix matrix2, matrix result);
 void SVD_matrix_trans(matrix in, matrix out);
 void SVD_matrix_copy(matrix in, matrix out);
-float SVD_matrix_dot(matrix matrix1, matrix matrix2, int row1, int col2);
 void SVD_matrix_rotation_angles(matrix matrix);
 bool SVD_matrix_equal(matrix matrix1, matrix matrix2);
 bool SVD_matrix_isDiagonal(matrix in);
@@ -18,7 +17,6 @@ void SVD_matrix_print(matrix in);
 #ifdef TEST
 void SVD_matrix_print(matrix m);
 void TEST_SVD_matrix_equal(void);
-void TEST_SVD_matrix_dot(void);
 void TEST_SVD_matrix_mul(void);
 void TEST_SVD_matrix_trans(void);
 void TEST_SVD_matrix_copy(void);
