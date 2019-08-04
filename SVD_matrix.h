9cebb96af5b75165a74bebc5e56ee9d9c6e2ed15
Matthew Wegener <mwegener@protonmail.com>
2019-08-04 11:52:50 -0700

    SIMD Transpose implementation

--- SVD_matrix.h
+++ SVD_matrix.h
@@ -25,12 +25,44 @@ static inline void SVD_matrix_mul(matrix matrix1, matrix matrix2, matrix result)
 
 static inline void SVD_matrix_trans(matrix in, matrix out)
 {
+    /*
   for ( int i = 0; i < N; i++ )
   {
     for ( int j = 0; j < N; j++ )
     {
       out[j][i] = in[i][j];
     }
+  }
+  */
+
+  float32x4x4_t temp_matrix = vld4q_f32(in[0][0]);
+
+  #ifdef TEST
+  if(in[0][0] == temp_matrix.val[0].val[0]){
+      printf("%f == %f\n", in[0][0],temp_matrix.val[0].val[0]);
+  }else{
+      printf("in[0][0] = %f, temp_matrix[0][0] = %f\n", in[0][0],temp_matrix.val[0].val[0]);
+  }
+  #endif
+
+  /*void vst1q_lane_f32(Scalar_t* N, Vector_t M, int n);
+  Scalar_t: float32_t
+  Vector_t: float32x4_t
+  int range: 0-3
+  Purpose: Store element n (lane) from vector M (array) into memory at address of scalar N
+
+  void vst1q_f32(Scalar_t* N, Vector_t M);
+  Scalar_t: float32_t
+  Vector_t: float32x4_t
+  Purpose: Store vector M into memory at address of scalar N
+
+  Following should store 1 row back into the out matrix
+  vst1q_lane(out[0][0],temp_matrix.val[0]);
+  */
+
+  //for can be unrolled later
+  for(int i = 0; i < N; i++){
+      vst1q_lane(out[i][0],temp_matrix.val[i]);
   }
 }
 
