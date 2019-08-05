0d99488852980f38873a24933c1948cc6ee7a364
Noah <ntrondeau@gmail.com>
2019-08-05 16:45:35 -0700

    Add print statements to arctan

--- SVD_math.h
+++ SVD_math.h
@@ -7,6 +7,10 @@
 #include <math.h>
 //#include <arm_neon.h>
 
+#ifdef TEST
+#include <stdio.h>
+#endif
+
 /* convert to and from floating point */
 #define TOFIX(d, q) ((int)( (d)*(double)(1<<(q)) ))
 #define TOFLT(a, q) ( (double)(a) / (double)(1<<(q)) )
@@ -70,22 +74,59 @@ static inline int32_t SVD_atan(int32_t y, int32_t x)
     int32_t div_res;
     int32_t ret;
 
+    ///////// Singularities ////////////
+    if (x == 0)
+    {
+        if ( y > 0)
+        {
+            return ATAN_QPI_2; // return PI/2
+        }
+
+        if (y < 0)
+        {
+            return -ATAN_QPI_2; // return -PI/2
+        }
+
+        if (y == 0)
+        {
+            /// THIS IS THE UNDEFINED SINGULARITY
+            /// We haven't dealt with this;
+            return 0;
+        }
+    }
+    ////////////////////////////////////
+
+
     y64_temp = (int64_t)y;
     x64_temp = (int64_t)x;
 
-    if (iABS(x) >= iABS(y))
+
+    printf("y64_temp = &llx\n", y64_temp);
+    printf("x64_temp = &llx\n", x64_temp);
+
+    if (iABS(x) > iABS(y))
     {
+        printf("x > y\n");
+
         y64_temp = y64_temp << ATANQ;
+        printf("y64_temp after shift = &llx\n", y64_temp);
         div_temp = y64_temp / x64_temp;
+        printf("div_temp = &llx\n", div_temp);
         div_res = (int32_t)div_temp;
+        printf("div_res = &x\n", div_res);
 
         ret = arctan(div_res);
     }
     else
     {
+        print(" x not > y\n");
+
         x64_temp = x64_temp << ATANQ;
+        printf("x64_temp after shift = &llx\n", x64_temp);
         div_temp = x64_temp / y64_temp;
+        printf("div_temp = &llx\n", div_temp);
         div_res = (int32_t)div_temp;
+        printf("div_res = &x\n", div_res);
 
         
         ret = arctan(div_res);
@@ -101,6 +142,7 @@ static inline int32_t SVD_atan(int32_t y, int32_t x)
         
     }
 
+    print("ret = %d\n", ret);
     return ret;
     
 }
