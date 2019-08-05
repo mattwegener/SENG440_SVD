c3d70b24daae146e83df879a027bb198f96bdaf2
Noah <ntrondeau@gmail.com>
2019-08-05 15:03:28 -0700

    Replace Arctan with  fixed point
    use cast to int64_t for high precision division

--- SVD_math.c
+++ SVD_math.c
@@ -2,30 +2,30 @@
 #include "SVD_math.h"
 
 // INCLUSION OF math.h REQUIRES -lm COMPILER FLAG
-#include <math.h>
-
-matrix_elem SVD_cos(matrix_elem x){
-  matrix_elem result =  cosf(x);
-  return result;
-}
-
-matrix_elem SVD_sin(matrix_elem x){
-  matrix_elem result =  sinf(x);
-  return result;
-}
-
-matrix_elem SVD_atan(matrix_elem y, matrix_elem x){
-    return atanf(y/x);
-}
-
-matrix_elem SVD_abs(matrix_elem x)
-{
-    return fabs(x);
-}
 
 #ifdef TEST
+#include <math.h>
+#include <assert.h>
+#include <stdio.h>
+
 void TEST_SVD_math()
 {
-  // TODO
+  int32_t atan_arg[100];
+  float ax = -1.0; // 0.02
+
+  for (int i = 0; i < 100; i++)
+  {
+
+      atan_arg[i] = TOFIX(ax, ATANQ);
+      ax += 0.02;
+  }
+  // test the arctan
+    for (int i = 0; i < 100; i++)
+    {
+      int32_t y = arctan(atan_arg[i]);
+      float yf = atanf(TOFLT(atan_arg[i], ATANQ));
+      printf("x = %f, y = %f, yf = %f\n", TOFLT(atan_arg[i], ATANQ), TOFLT(y, ATANQ), yf);
+      assert( fabs(yf - TOFLT(y,ATANQ)) <=  0.019);
+    }
 }
 #endif
--- SVD_math.h
+++ SVD_math.h
@@ -3,11 +3,114 @@
 #define SVD_MATH_H
 
 #include "SVD_defs.h"
+#include <stdint.h>
+#include <math.h>
+//#include <arm_neon.h>
 
-matrix_elem SVD_sin(matrix_elem x);
-matrix_elem SVD_cos(matrix_elem x);
-matrix_elem SVD_atan(matrix_elem y, matrix_elem x);
-matrix_elem SVD_abs(matrix_elem x);
+/* convert to and from floating point */
+#define TOFIX(d, q) ((int)( (d)*(double)(1<<(q)) ))
+#define TOFLT(a, q) ( (double)(a) / (double)(1<<(q)) )
+
+/*
+static inline matrix_elem SVD_cos(matrix_elem x){
+  matrix_elem result =  cosf(x);
+  return result;
+}
+
+static inline matrix_elem SVD_sin(matrix_elem x){
+  matrix_elem result =  sinf(x);
+  return result;
+}
+*/
+#define iABS(x)     ((x > 0) ? (x) : (-x))
+#define ATANQ       (14)
+#define ATAN_QPI_2      (25735) // Q14
+#define ATAN_1          (16384) // Q14
+#define ATAN_0_5        (8192) // Q14
+
+#define ATAN_COEFF_0644     (10551) // Q14 format
+#define ATAN_COEFF_0928     (15204) //Q14 format
+#define ATAN_COEFF_0142     (38117834) // Q28 format
+
+
+static inline int32_t arctan(int32_t x)
+{
+    int32_t temp;
+
+    if ( x > ATAN_0_5) // we know the argument will be less than 1, no need to check the upper bound
+    {
+        temp = ATAN_COEFF_0142;
+        temp += (ATAN_COEFF_0644 * x);
+        temp = temp >> ATANQ;
+        return temp;
+    }
+    
+    if (x >= -ATAN_0_5 && x <= ATAN_0_5)
+    {
+        return (ATAN_COEFF_0928 * x) >> ATANQ;
+    }
+
+    if (x < -ATAN_0_5)
+    {
+        temp = -ATAN_COEFF_0142; // minus sign important!
+        temp += (ATAN_COEFF_0644 * x);
+        temp = temp >> ATANQ;
+        return temp;
+    }
+
+    return 0; //this is dead code, placed to elim compiler warnings
+}
+
+// preconditions: y, x are in Q.14 format
+// returns it in Q14 format
+static inline int32_t SVD_atan(int32_t y, int32_t x)
+{
+    int64_t y64_temp, x64_temp;
+    int64_t div_temp;
+    int32_t div_res;
+    int32_t ret;
+
+    y64_temp = (int16_t)y;
+    x64_temp = (int64_t)x;
+
+    if (iABS(x) >= iABS(y))
+    {
+        y64_temp = y64_temp << ATANQ;
+        div_temp = y64_temp / x64_temp;
+        div_res = (int32_t)div_temp;
+
+        ret = arctan(div_res);
+    }
+    else
+    {
+        x64_temp = x64_temp << ATANQ;
+        div_temp = x64_temp / y64_temp;
+        div_res = (int32_t)div_temp;
+
+        
+        ret = arctan(div_res);
+
+        if (ret >= 0)
+        {
+            ret = ATAN_QPI_2 - ret;
+        }
+        else
+        {
+            ret = -ATAN_QPI_2 - ret;
+        }
+        
+    }
+
+    return ret;
+    
+}
+
+/*
+static inline matrix_elem SVD_abs(matrix_elem x)
+{
+    return fabs(x);
+}
+*/
 
 #ifdef TEST
 void TEST_SVD_math(void);
