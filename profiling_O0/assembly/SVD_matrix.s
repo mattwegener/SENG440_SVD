d650cd83271474296b74c85f88c1d33ea28e7622
root <root@localhost.localdomain>
2019-08-06 15:41:37 -0700

    Profile fixed-point algo

--- profiling_O0/assembly/SVD_matrix.s
+++ profiling_O0/assembly/SVD_matrix.s
@@ -11,5 +11,117 @@
 	.eabi_attribute 18, 4
 	.file	"SVD_matrix.c"
 	.text
+	.align	2
+	.global	SVD_matrix_int_to_fix
+	.arch armv7-a
+	.syntax unified
+	.arm
+	.fpu neon
+	.type	SVD_matrix_int_to_fix, %function
+SVD_matrix_int_to_fix:
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	sub	sp, sp, #20
+	str	r0, [fp, #-16]
+	mov	r3, #0
+	str	r3, [fp, #-8]
+	b	.L2
+.L5:
+	mov	r3, #0
+	str	r3, [fp, #-12]
+	b	.L3
+.L4:
+	ldr	r3, [fp, #-8]
+	lsl	r3, r3, #4
+	ldr	r2, [fp, #-16]
+	add	r3, r2, r3
+	ldr	r2, [fp, #-12]
+	ldr	r2, [r3, r2, lsl #2]
+	ldr	r3, [fp, #-8]
+	lsl	r3, r3, #4
+	ldr	r1, [fp, #-16]
+	add	r3, r1, r3
+	lsl	r1, r2, #8
+	ldr	r2, [fp, #-12]
+	str	r1, [r3, r2, lsl #2]
+	ldr	r3, [fp, #-12]
+	add	r3, r3, #1
+	str	r3, [fp, #-12]
+.L3:
+	ldr	r3, [fp, #-12]
+	cmp	r3, #3
+	ble	.L4
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #1
+	str	r3, [fp, #-8]
+.L2:
+	ldr	r3, [fp, #-8]
+	cmp	r3, #3
+	ble	.L5
+	nop
+	add	sp, fp, #0
+	@ sp needed
+	ldr	fp, [sp], #4
+	bx	lr
+	.size	SVD_matrix_int_to_fix, .-SVD_matrix_int_to_fix
+	.align	2
+	.global	SVD_matrix_fix_to_int
+	.syntax unified
+	.arm
+	.fpu neon
+	.type	SVD_matrix_fix_to_int, %function
+SVD_matrix_fix_to_int:
+	@ args = 0, pretend = 0, frame = 16
+	@ frame_needed = 1, uses_anonymous_args = 0
+	@ link register save eliminated.
+	str	fp, [sp, #-4]!
+	add	fp, sp, #0
+	sub	sp, sp, #20
+	str	r0, [fp, #-16]
+	mov	r3, #0
+	str	r3, [fp, #-8]
+	b	.L7
+.L10:
+	mov	r3, #0
+	str	r3, [fp, #-12]
+	b	.L8
+.L9:
+	ldr	r3, [fp, #-8]
+	lsl	r3, r3, #4
+	ldr	r2, [fp, #-16]
+	add	r3, r2, r3
+	ldr	r2, [fp, #-12]
+	ldr	r3, [r3, r2, lsl #2]
+	add	r2, r3, #128
+	ldr	r3, [fp, #-8]
+	lsl	r3, r3, #4
+	ldr	r1, [fp, #-16]
+	add	r3, r1, r3
+	asr	r1, r2, #8
+	ldr	r2, [fp, #-12]
+	str	r1, [r3, r2, lsl #2]
+	ldr	r3, [fp, #-12]
+	add	r3, r3, #1
+	str	r3, [fp, #-12]
+.L8:
+	ldr	r3, [fp, #-12]
+	cmp	r3, #3
+	ble	.L9
+	ldr	r3, [fp, #-8]
+	add	r3, r3, #1
+	str	r3, [fp, #-8]
+.L7:
+	ldr	r3, [fp, #-8]
+	cmp	r3, #3
+	ble	.L10
+	nop
+	add	sp, fp, #0
+	@ sp needed
+	ldr	fp, [sp], #4
+	bx	lr
+	.size	SVD_matrix_fix_to_int, .-SVD_matrix_fix_to_int
 	.ident	"GCC: (GNU) 8.2.1 20180801 (Red Hat 8.2.1-2)"
 	.section	.note.GNU-stack,"",%progbits
--- profiling_O3/assembly/SVD_matrix.s
+++ profiling_O3/assembly/SVD_matrix.s
@@ -11,5 +11,65 @@
 	.eabi_attribute 18, 4
 	.file	"SVD_matrix.c"
 	.text
+	.align	2
+	.global	SVD_matrix_int_to_fix
+	.arch armv7-a
+	.syntax unified
+	.arm
+	.fpu neon
+	.type	SVD_matrix_int_to_fix, %function
+SVD_matrix_int_to_fix:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	@ link register save eliminated.
+	vld1.32	{q8}, [r0]
+	mov	r3, r0
+	vshl.i32	q8, q8, #8
+	vst1.32	{q8}, [r3]!
+	vld1.32	{q8}, [r3]
+	vshl.i32	q8, q8, #8
+	vst1.32	{q8}, [r3]
+	add	r3, r0, #32
+	vld1.32	{q8}, [r3]
+	vshl.i32	q8, q8, #8
+	vst1.32	{q8}, [r3]
+	add	r0, r0, #48
+	vld1.32	{q8}, [r0]
+	vshl.i32	q8, q8, #8
+	vst1.32	{q8}, [r0]
+	bx	lr
+	.size	SVD_matrix_int_to_fix, .-SVD_matrix_int_to_fix
+	.align	2
+	.global	SVD_matrix_fix_to_int
+	.syntax unified
+	.arm
+	.fpu neon
+	.type	SVD_matrix_fix_to_int, %function
+SVD_matrix_fix_to_int:
+	@ args = 0, pretend = 0, frame = 0
+	@ frame_needed = 0, uses_anonymous_args = 0
+	@ link register save eliminated.
+	vld1.32	{q9}, [r0]
+	vmov.i32	q10, #128  @ v4si
+	vadd.i32	q9, q9, q10
+	mov	r3, r0
+	vshr.s32	q9, q9, #8
+	vst1.32	{q9}, [r3]!
+	vld1.32	{q9}, [r3]
+	vadd.i32	q9, q9, q10
+	vshr.s32	q9, q9, #8
+	vst1.32	{q9}, [r3]
+	add	r3, r0, #32
+	vld1.32	{q9}, [r3]
+	vadd.i32	q9, q9, q10
+	vshr.s32	q9, q9, #8
+	vst1.32	{q9}, [r3]
+	add	r0, r0, #48
+	vld1.32	{q8}, [r0]
+	vadd.i32	q8, q8, q10
+	vshr.s32	q8, q8, #8
+	vst1.32	{q8}, [r0]
+	bx	lr
+	.size	SVD_matrix_fix_to_int, .-SVD_matrix_fix_to_int
 	.ident	"GCC: (GNU) 8.2.1 20180801 (Red Hat 8.2.1-2)"
 	.section	.note.GNU-stack,"",%progbits
