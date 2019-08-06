69765e6f6e7272f08280a904307dfd8de75f7178
Noah <ntrondeau@gmail.com>
2019-08-06 12:10:50 -0700

    Add missing ifdef guard

--- SVD.c
+++ SVD.c
@@ -159,7 +159,9 @@ void SVD_decompose(matrix M /*IN*/, matrix U /*OUT*/, matrix S /*OUT*/, matrix V
         }//end for
 	    if (sweeps == 5)
         {
+            #ifdef TEST
             fflush(stdout);
+            #endif
             return;
         }
 	    sweeps++;
