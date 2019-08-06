776174bd7d663d0c628b8d8ef0e490c8e448dff6
Noah <ntrondeau@gmail.com>
2019-08-05 22:09:45 -0700

    Functioning sin and cos
    Using Q12

--- SVD_math.c
+++ SVD_math.c
@@ -4,6 +4,7 @@
 // INCLUSION OF math.h REQUIRES -lm COMPILER FLAG
 
 #ifdef TEST
+#include <math.h>
 #include <assert.h>
 #include <stdio.h>
 
@@ -12,19 +13,51 @@ void TEST_SVD_math()
   int32_t atan_arg[100];
   float ax = -1.0; // 0.02
 
+  int32_t sincos_arg[100];
+  float sx = -M_PI;
+  
   for (int i = 0; i < 100; i++)
   {
+      int32_t val = TOFIX(sx, SINCOS_Q1);
+      if (val >= SINCOS_PI)
+        val = SINCOS_PI;
+      else if (val <= -SINCOS_PI)
+        val = -SINCOS_PI;
+      
+      sincos_arg[i] = val;
+      sx += (2 * M_PI / 100.0);
+  }
 
+  for (int i = 0; i < 100; i++)
+  {
       atan_arg[i] = TOFIX(ax, ATANQ);
       ax += 0.02;
   }
   // test the arctan
-    for (int i = 0; i < 100; i++)
-    {
-      int32_t y = arctan(atan_arg[i]);
-      float yf = atanf(TOFLT(atan_arg[i], ATANQ));
-      //printf("x = %f, y = %f, yf = %f\n", TOFLT(atan_arg[i], ATANQ), TOFLT(y, ATANQ), yf);
-      assert( fabs(yf - TOFLT(y,ATANQ)) <=  0.019);
-    }
+  for (int i = 0; i < 100; i++)
+  {
+    int32_t y = arctan(atan_arg[i]);
+    float yf = atanf(TOFLT(atan_arg[i], ATANQ));
+    //printf("x = %f, y = %f, yf = %f\n", TOFLT(atan_arg[i], ATANQ), TOFLT(y, ATANQ), yf);
+    assert( fabs(yf - TOFLT(y,ATANQ)) <=  0.019);
+  }
+
+  for (int i = 0; i < 100; i++)
+  {
+    int32_t y = SVD_cos(sincos_arg[i]);
+    float yf = cosf(TOFLT(sincos_arg[i], SINCOS_Q1));
+
+    //printf("x = %f, y = %f, yf = %f\n", TOFLT(sincos_arg[i], SINCOS_Q1), TOFLT(y, SINCOS_Q1), yf);
+    assert( fabs(yf - TOFLT(y,SINCOS_Q1)) <=  0.01);
+  }
+
+  for (int i = 0; i < 100; i++)
+  {
+    int32_t y = SVD_sin(sincos_arg[i]);
+    float yf = sinf(TOFLT(sincos_arg[i], SINCOS_Q1));
+
+    //printf("x = %f, y = %f, yf = %f\n", TOFLT(sincos_arg[i], SINCOS_Q1), TOFLT(y, SINCOS_Q1), yf);
+    assert( fabs(yf - TOFLT(y,SINCOS_Q1)) <=  0.01);
+  }  
 }
 #endif
--- SVD_math.h
+++ SVD_math.h
@@ -9,24 +9,250 @@
 
 #ifdef TEST
 #include <stdio.h>
+#include <assert.h>
 #endif
 
 /* convert to and from floating point */
+#define iABS(x)     ((x > 0) ? (x) : (-x))
 #define TOFIX(d, q) ((int)( (d)*(double)(1<<(q)) ))
 #define TOFLT(a, q) ( (double)(a) / (double)(1<<(q)) )
 
+#define SINCOS_Q1		(12)
+#define SINCOS_Q2		(24)
+#define SINCOS_1		(4096)
+#define SINCOS_PI		(12867)
+#define SINCOS_PI2		(6433)
 
-static inline matrix_elem SVD_cos(matrix_elem x){
-  matrix_elem result =  cosf(x);
-  return result;
+#define SIN_SLOPE_1		(4049)
+#define SIN_SLOPE_2		(3773)
+#define SIN_SLOPE_3		(3240)
+#define SIN_SLOPE_4		(2486)
+#define SIN_SLOPE_5		(1810)
+#define SIN_SLOPE_6		(1315)
+#define SIN_SLOPE_7		(798)
+#define SIN_SLOPE_8		(267)
+
+#define SIN_CONST_1		(0)
+#define SIN_CONST_2		(295918)
+#define SIN_CONST_3		(1439257)
+#define SIN_CONST_4		(3864647)
+#define SIN_CONST_5		(6764445)
+#define SIN_CONST_6		(9151348)
+#define SIN_CONST_7		(11924162)
+#define SIN_CONST_8		(15054839)
+
+#define COS_SLOPE_1		(-268)
+#define COS_SLOPE_2		(-799)
+#define COS_SLOPE_3		(-1316)
+#define COS_SLOPE_4		(-1811)
+#define COS_SLOPE_5		(-2487)
+#define COS_SLOPE_6		(-3241)
+#define COS_SLOPE_7		(-3774)
+#define COS_SLOPE_8		(-4050)
+
+#define COS_CONST_1		(16777216)
+#define COS_CONST_2		(17061822)
+#define COS_CONST_3		(17616385)
+#define COS_CONST_4		(18412020)
+#define COS_CONST_5		(19861919)
+#define COS_CONST_6		(22287308)
+#define COS_CONST_7		(24573987)
+#define COS_CONST_8		(26053578)
+
+#define SIN_LIM_1		(1072)
+#define SIN_LIM_2		(2144)
+#define SIN_LIM_3		(3216)
+#define SIN_LIM_4		(4289)
+#define SIN_LIM_5		(4825)
+#define SIN_LIM_6		(5361)
+#define SIN_LIM_7		(5897)
+#define SIN_LIM_8		(6433)
+
+#define COS_LIM_1		(536)
+#define COS_LIM_2		(1072)
+#define COS_LIM_3		(1608)
+#define COS_LIM_4		(2144)
+#define COS_LIM_5		(3216)
+#define COS_LIM_6		(4289)
+#define COS_LIM_7		(5361)
+#define COS_LIM_8		(6433)
+
+// precondition: x in Q14
+// x between -pi and +pi
+static inline int32_t SVD_cos(int32_t x)
+{
+    #ifdef TEST
+        // assert the range of the input argument
+        assert(x <= SINCOS_PI);
+        assert(x >= -SINCOS_PI);
+    #endif
+
+    int32_t ret;
+    int32_t range_scale = 1;
+    int32_t slope;
+    int32_t constant;
+    // cosine is even, cos(x) = cos(-x)
+    x = iABS(x);
+
+    // easy returns:
+    if (x == 0)
+        return SINCOS_1; // return 1
+    
+    if (x == SINCOS_PI2)
+        return 0; // return 0
+
+    if (x == SINCOS_PI)
+        return -SINCOS_1; // return -1
+
+    // get x in 0 to pi/2
+    // mirror across pi/2
+    if (x > SINCOS_PI2)
+    {
+        x = SINCOS_PI - x;
+        range_scale = -1; // save this so we know to negate the result later
+    }
+
+    // now check ranges in best way possible
+
+    if (x < COS_LIM_1)
+    {
+        slope = COS_SLOPE_1;
+        constant = COS_CONST_1;
+    }
+    else if (x < COS_LIM_2)
+    {
+        slope = COS_SLOPE_2;
+        constant = COS_CONST_2;
+    }
+    else if (x < COS_LIM_3)
+    {
+        slope = COS_SLOPE_3;
+        constant = COS_CONST_3;
+    }
+    else if (x < COS_LIM_4)
+    {
+        slope = COS_SLOPE_4;
+        constant = COS_CONST_4;
+    }
+    else if (x < COS_LIM_5)
+    {
+        slope = COS_SLOPE_5;
+        constant = COS_CONST_5;
+    }
+    else if (x < COS_LIM_6)
+    {
+        slope = COS_SLOPE_6;
+        constant = COS_CONST_6;
+    }
+    else if (x < COS_LIM_7)
+    {
+        slope = COS_SLOPE_7;
+        constant = COS_CONST_7;
+    }
+    else if (x < COS_LIM_8)
+    {
+        slope = COS_SLOPE_8;
+        constant = COS_CONST_8;
+    }
+
+    ret = (slope * x);
+    // ret is now in format SINCOS_Q2
+    ret += constant; // add the constant, pre-encoded in SINCOS_Q2
+    ret = ret >> SINCOS_Q1; // shift back down to original Q format
+
+    return range_scale * ret; // return with sign appropriate to range
 }
 
-static inline matrix_elem SVD_sin(matrix_elem x){
-  matrix_elem result =  sinf(x);
-  return result;
+static inline int32_t SVD_sin(int32_t x)
+{
+    #ifdef TEST
+        // assert the range of the input argument
+        assert(x <= SINCOS_PI);
+        assert(x >= -SINCOS_PI);
+    #endif
+
+    int32_t ret;
+    int32_t range_scale = 1;
+    int32_t slope;
+    int32_t constant;
+
+    // sine is odd, but symmetric
+    if (x < 0)
+    {
+        x = iABS(x);
+        range_scale = -1;
+    }
+
+    // easy returns:
+    if (x == 0)
+        return 0; // return 0
+    
+    if (x == SINCOS_PI2)
+        return range_scale * SINCOS_1; // return +/-1
+
+    if (x == SINCOS_PI)
+        return 0;
+
+    // get x in 0 to pi/2
+    // mirror across pi/2
+    if (x > SINCOS_PI2)
+    {
+        x = SINCOS_PI - x;
+        // unlike cosine, here we don't need to track the range_scale
+        // since it positive on both sides of pi/2
+    }
+
+    // now check ranges in best way possible
+
+    if (x < SIN_LIM_1)
+    {
+        slope = SIN_SLOPE_1;
+        constant = SIN_CONST_1;
+    }
+    else if (x < SIN_LIM_2)
+    {
+        slope = SIN_SLOPE_2;
+        constant = SIN_CONST_2;
+    }
+    else if (x < SIN_LIM_3)
+    {
+        slope = SIN_SLOPE_3;
+        constant = SIN_CONST_3;
+    }
+    else if (x < SIN_LIM_4)
+    {
+        slope = SIN_SLOPE_4;
+        constant = SIN_CONST_4;
+    }
+    else if (x < SIN_LIM_5)
+    {
+        slope = SIN_SLOPE_5;
+        constant = SIN_CONST_5;
+    }
+    else if (x < SIN_LIM_6)
+    {
+        slope = SIN_SLOPE_6;
+        constant = SIN_CONST_6;
+    }
+    else if (x < SIN_LIM_7)
+    {
+        slope = SIN_SLOPE_7;
+        constant = SIN_CONST_7;
+    }
+    else if (x < SIN_LIM_8)
+    {
+        slope = SIN_SLOPE_8;
+        constant = SIN_CONST_8;
+    }
+
+    ret = (slope * x);
+    // ret is now in format SINCOS_Q2
+    ret += constant; // add the constant, pre-encoded in SINCOS_Q2
+    ret = ret >> SINCOS_Q1; // shift back down to original Q format
+
+    return range_scale * ret; // return with sign appropriate to range
 }
 
-#define iABS(x)     ((x > 0) ? (x) : (-x))
 #define ATANQ       (14)
 #define ATAN_QPI_2      (25735) // Q14
 #define ATAN_1          (16384) // Q14
@@ -39,6 +265,12 @@ static inline matrix_elem SVD_sin(matrix_elem x){
 
 static inline int32_t arctan(int32_t x)
 {
+    #ifdef TEST
+        // assert the range of the input argument
+        assert(x <= ATAN_1);
+        assert(x >= -ATAN_1);
+    #endif
+
     int32_t temp;
 
     if ( x > ATAN_0_5) // we know the argument will be less than 1, no need to check the upper bound
@@ -69,6 +301,12 @@ static inline int32_t arctan(int32_t x)
 // returns it in Q14 format
 static inline int32_t SVD_atan(int32_t y, int32_t x)
 {
+    #ifdef TEST
+        // assert the values of the input argument
+        if (y == 0) assert( x != 0);
+        if (x == 0) assert( y != 0);
+    #endif
+
     int64_t y64_temp, x64_temp;
     int64_t div_temp;
     int32_t div_res;
